9028888909e04eb41d6118a7d8f83c6b0f77e446
Nils pettersson <nils-pettsson@outlook.com>
2021-04-15 22:45:17 +0200

    added basic shader and gl array buffer

--- Project1/src/Application.cpp
+++ Project1/src/Application.cpp
@@ -1,22 +1,110 @@
 #include <iostream>
 #include <crtdbg.h>
 #include "Window.h"
+#include <fstream>
+#include <string>
+#include <sstream>
 
+struct ShaderProgramSouce {
+    std::string vertexSource;
+    std::string fragmentSource;
+};
+
+static ShaderProgramSouce ParseShader(const std::string& filepath) {
+    std::ifstream stream(filepath);
+
+    enum class ShaderType {
+        NONE = -1, VERTEX = 0, FRAGMENT = 1
+    };
+
+    ShaderType type = ShaderType::NONE;
+
+    std::string line;
+    std::stringstream shaderStream[2];
+    while (std::getline(stream, line)) {
+        if (line.find("#shader") != std::string::npos) {
+            if (line.find("vertex") != std::string::npos) {
+                type = ShaderType::VERTEX;
+            }
+            else if (line.find("fragment") != std::string::npos) {
+                type = ShaderType::FRAGMENT;
+            }
+        }
+        else {
+            shaderStream[(int)type] << line << "\n";
+        }
+    }
+    return { shaderStream[0].str(), shaderStream[1].str() };
+}
+
+static unsigned int compileShader(std::string& source, unsigned int type) {
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE) {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "shader error" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int createShader(std::string& vertexShader, std::string& fragmentShader) {
+    unsigned int program = glCreateProgram();
+    unsigned int vs = compileShader(vertexShader, GL_VERTEX_SHADER);
+    unsigned int fs = compileShader(fragmentShader, GL_FRAGMENT_SHADER);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
 
 int main(void) {
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-    Window window = Window();
+    Window window = Window(600, 600);
+
+    float positions[6] = {
+        -0.5f, -0.5f,
+        0.0f, 0.5f,
+        0.5f, -0.5f,
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    ShaderProgramSouce source = ParseShader("res/shaders/main.shader");
+    unsigned int shader = createShader(source.vertexSource, source.fragmentSource);
+    glUseProgram(shader);
 
     while (!window.shouldClose()) {
         window.drawInit();
 
-        glBegin(GL_TRIANGLES);
-        glVertex2d(-0.5f, -0.5f);
-        glVertex2d(0.0f, 0.5f);
-        glVertex2d(0.5f, -0.5f);
-        glEnd();
-        
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         window.clean();
     }
     window.destroy();
