29d2d3122461fc927496637a4628c4b2a4616c8f
Nils pettersson <nils-pettsson@outlook.com>
2021-04-15 23:17:47 +0200

    removed functions and added shader object

--- Project1/src/Application.cpp
+++ Project1/src/Application.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <crtdbg.h>
 #include "Window.h"
+#include "shader/Shader.h"
 #include <fstream>
 #include <string>
 #include <sstream>
@@ -37,45 +38,6 @@ static ShaderProgramSouce ParseShader(const std::string& filepath) {
     return { shaderStream[0].str(), shaderStream[1].str() };
 }
 
-static unsigned int compileShader(std::string& source, unsigned int type) {
-    unsigned int id = glCreateShader(type);
-    const char* src = source.c_str();
-    glShaderSource(id, 1, &src, nullptr);
-    glCompileShader(id);
-
-    int result;
-    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-    if (result == GL_FALSE) {
-        int length;
-        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-        char* message = (char*)alloca(length * sizeof(char));
-        glGetShaderInfoLog(id, length, &length, message);
-        std::cout << "shader error" << std::endl;
-        std::cout << message << std::endl;
-        glDeleteShader(id);
-
-        return 0;
-    }
-
-    return id;
-}
-
-static unsigned int createShader(std::string& vertexShader, std::string& fragmentShader) {
-    unsigned int program = glCreateProgram();
-    unsigned int vs = compileShader(vertexShader, GL_VERTEX_SHADER);
-    unsigned int fs = compileShader(fragmentShader, GL_FRAGMENT_SHADER);
-
-    glAttachShader(program, vs);
-    glAttachShader(program, fs);
-    glLinkProgram(program);
-    glValidateProgram(program);
-
-    glDeleteShader(vs);
-    glDeleteShader(fs);
-
-    return program;
-}
-
 int main(void) {
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
@@ -95,10 +57,10 @@ int main(void) {
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
-
     ShaderProgramSouce source = ParseShader("res/shaders/main.shader");
-    unsigned int shader = createShader(source.vertexSource, source.fragmentSource);
-    glUseProgram(shader);
+
+
+    Shader shader(source.vertexSource, source.fragmentSource);
 
     while (!window.shouldClose()) {
         window.drawInit();
