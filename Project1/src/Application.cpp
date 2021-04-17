79a5094e90ccac5e5737f1a9faeb19a4e37e6e1e
Nils pettersson <nils-pettsson@outlook.com>
2021-04-17 11:31:04 +0200

    removed shader code from application file

--- Project1/src/Application.cpp
+++ Project1/src/Application.cpp
@@ -6,38 +6,6 @@
 #include <string>
 #include <sstream>
 
-struct ShaderProgramSouce {
-    std::string vertexSource;
-    std::string fragmentSource;
-};
-
-static ShaderProgramSouce ParseShader(const std::string& filepath) {
-    std::ifstream stream(filepath);
-
-    enum class ShaderType {
-        NONE = -1, VERTEX = 0, FRAGMENT = 1
-    };
-
-    ShaderType type = ShaderType::NONE;
-
-    std::string line;
-    std::stringstream shaderStream[2];
-    while (std::getline(stream, line)) {
-        if (line.find("#shader") != std::string::npos) {
-            if (line.find("vertex") != std::string::npos) {
-                type = ShaderType::VERTEX;
-            }
-            else if (line.find("fragment") != std::string::npos) {
-                type = ShaderType::FRAGMENT;
-            }
-        }
-        else {
-            shaderStream[(int)type] << line << "\n";
-        }
-    }
-    return { shaderStream[0].str(), shaderStream[1].str() };
-}
-
 int main(void) {
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
@@ -57,9 +25,8 @@ int main(void) {
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
-    ShaderProgramSouce source = ParseShader("res/shaders/main.shader");
 
-
+    Shader::ShaderProgramSouce source = Shader::parseShader("res/shaders/main.shader");
     Shader shader(source.vertexSource, source.fragmentSource);
 
     while (!window.shouldClose()) {
