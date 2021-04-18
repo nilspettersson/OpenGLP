1044d7f1053c53c97255cb6d201cf186e5a429b5
Nils pettersson <nils-pettsson@outlook.com>
2021-04-18 10:41:29 +0200

    added index buffer

--- Project1/src/Application.cpp
+++ Project1/src/Application.cpp
@@ -11,21 +11,31 @@ int main(void) {
 
     Window window = Window(600, 600);
 
-    float positions[6] = {
+    float positions[] = {
         -0.5f, -0.5f,
-        0.0f, 0.5f,
         0.5f, -0.5f,
+        0.5f, 0.5f,
+        -0.5f, 0.5f,
+    };
+
+    unsigned int indices[]{
+        0, 1, 2,
+        2, 3, 0
     };
 
     unsigned int buffer;
     glGenBuffers(1, &buffer);
-
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
+
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
 
     Shader shader("res/shaders/main.shader");
@@ -33,7 +43,7 @@ int main(void) {
     while (!window.shouldClose()) {
         window.drawInit();
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6 , GL_UNSIGNED_INT, nullptr);
 
         window.clean();
     }
