03216c8ac8eaf9c66bae597b388c4fc2ebc4f626
Nils pettersson <nils-pettsson@outlook.com>
2021-04-21 22:12:22 +0200

    changed names of ids

--- Project1/src/model/Vao.h
+++ Project1/src/model/Vao.h
@@ -5,11 +5,12 @@
 
 
 class Vao {
-	bool dynamicDraw;
-	unsigned int buffer;
+	unsigned int vaoArrayId;
 	std::vector<int> vertexLayout;
 	int vertexSize;
+	unsigned int vaoElementId;
+	void setVertexSize();
 
 public:
-	Vao(std::vector<int> vertexLayout, bool dynamicDraw);
+	Vao(std::vector<int> vertexLayout);
 };
