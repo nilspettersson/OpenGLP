5931500424cd95fc5d4b109036ab0d6196097bdc
Nils pettersson <nils-pettsson@outlook.com>
2021-04-21 22:13:46 +0200

    added setVertexSize function

--- Project1/src/model/Vao.cpp
+++ Project1/src/model/Vao.cpp
@@ -1,11 +1,10 @@
 #include "Vao.h"
 #include <iostream>
 
-Vao::Vao(std::vector<int> vertexLayout, bool dynamicDraw) {
-	this->dynamicDraw = dynamicDraw;
+Vao::Vao(std::vector<int> vertexLayout) {
 	this->vertexLayout = vertexLayout;
 
-	this->vertexSize = 0;
+	Vao::setVertexSize();
 
 	float positions[] = {
 		-0.5f, -0.5f,
@@ -19,28 +18,25 @@ Vao::Vao(std::vector<int> vertexLayout, bool dynamicDraw) {
 		2, 3, 0
 	};
 
-	/*for (int i = 0; i < this->vertexLayout.size(); i++) {
-		std::cout << "array: " << this->vertexLayout[i] << std::endl;
-	}*/
 
-	glGenBuffers(1, &this->buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, this->buffer);
+	glGenBuffers(1, &this->vaoArrayId);
+	glBindBuffer(GL_ARRAY_BUFFER, this->vaoArrayId);
 	glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
-	for (int i = 0; i < this->vertexLayout.size(); i++) {
-		this->vertexSize += this->vertexLayout[i] * sizeof(float);
-	}
 
 	for (int i = 0; i < this->vertexLayout.size(); i++) {
 		glEnableVertexAttribArray(i);
 		glVertexAttribPointer(i, this->vertexLayout[i], GL_FLOAT, GL_FALSE, this->vertexSize, 0);
 	}
 
-	/*glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);*/
-
-	unsigned int ibo;
-	glGenBuffers(1, &ibo);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glGenBuffers(1, &this->vaoElementId);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->vaoElementId);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+}
+
+void Vao::setVertexSize() {
+	this->vertexSize = 0;
+	for (int i = 0; i < this->vertexLayout.size(); i++) {
+		this->vertexSize += this->vertexLayout[i] * sizeof(float);
+	}
 }
