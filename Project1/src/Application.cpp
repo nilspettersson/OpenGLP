02e0e1fc0014d78587c4359a2992fd56b37625b5
Nils pettersson <nils-pettsson@outlook.com>
2021-04-24 16:37:55 +0200

    testing uniform

--- Project1/src/Application.cpp
+++ Project1/src/Application.cpp
@@ -22,11 +22,11 @@ int main(void) {
         0, 1, 2,
         2, 3, 0
     };
-
     Vao vao({ 2 }, indices, 6, positions, 4);
 
-
     Shader shader("res/shaders/main.shader");
+    shader.setUniform4f("color", 0.0, 1.0, 0.0, 1.0);
+
     while (!window.shouldClose()) {
         window.drawInit();
 
