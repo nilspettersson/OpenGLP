7d4a6f74ceb76b9c80243d2bcdf171bed3c63fec
Nils pettersson <nils-pettsson@outlook.com>
2021-05-04 13:01:00 +0200

    testing textures

--- Project1/src/Application.cpp
+++ Project1/src/Application.cpp
@@ -7,31 +7,39 @@
 #include "window/Window.h"
 #include "shader/Shader.h"
 #include "camera/Camera.h"
+#include "texture/Texture.h";
 
 int main(void) {
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-    Window window = Window(600, 600);
-    glp::Camera2d camera = glp::Camera2d(600, 600);
-    camera.setX(-200);
-    camera.setY(-200);
+    Window window = Window(1000, 1000);
+    glp::Camera2d camera = glp::Camera2d(1, 1);
+    camera.setX(0);
+    camera.setY(0);
 
-    float positions[] = {
-        -0.5f, -0.5f,
-        0.5f, -0.5f,
-        0.5f, 0.5f,
-        -0.5f, 0.5f,
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f, 0.0f,
+        0.5f, -0.5f, 1.0f, 0.0f,
+        0.5f, 0.5f, 1.0f, 1.0f,
+        -0.5f, 0.5f, 0.0f, 1.0f
     };
     unsigned int indices[]{
         0, 1, 2,
         2, 3, 0
     };
-    glp::Vao vao({ 2 }, indices, 6, positions, 4);
+    glp::Vao vao({2, 2}, indices, 6, vertices, 4);
 
     glp::Shader shader("res/shaders/main.shader");
     shader.setUniform4f("color", 0.0, 1.0, 0.0, 1.0);
 
     shader.setUniformMat4f("u_mvp", camera.getProjection());
+
+    glp::Texture texture("res/textures/test.png", glp::Texture::FILTER::NEAREST);
+    texture.bind();
+    shader.setUniform1i("u_texture", 0);
+
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_BLEND);
     
     while (!window.shouldClose()) {
         window.drawInit();
