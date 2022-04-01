1a8c915790d95846ece383fb8c8e219bf8ecea07
Nils pettersson <nils-pettsson@outlook.com>
2022-04-01 21:38:46 +0200

    added header with all library files

---
+++ openGLP/src/OpenGLP.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "model/Vao.h"
+#include "window/Window.h"
+#include "shader/Shader.h"
+#include "camera/Camera.h"
+#include "texture/Texture.h";
+#include "rendering/Renderer.h"
+#include "entity/Entity.h";
+#include "util/Util.h"
+#include "model/Mesh.h"
